2c001cec52b8ae00904597e849d16caabdf9b113
KodexDigital <33692534+KodexDigital@users.noreply.github.com>
2020-07-29 09:02:37 +0100

    Added pseudo deletable interface

--- System.Model/PropertyService.cs
+++ System.Model/PropertyService.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Model.DB;
+using System.Repository.IRepository;
 using System.Text;
 
 namespace System.Model
 {
 	[DisplayName("Create new property service")]
-	public class PropertyService : BaseModel
+	public class PropertyService : BaseModel, IPseudoDeletable
 	{
+		public bool Delete { get; set; }
 	}
 }
